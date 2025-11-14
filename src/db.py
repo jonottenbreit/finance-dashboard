27d5dc5a653d2e95d502ca6afe79f62742f1e886
jonottenbreit <jo136136@gmail.com>
2025-11-14 09:04:06 -0500

    Update db.py

--- src/db.py
+++ src/db.py
@@ -4,3 +4,13 @@ DB_PATH = r"C:\Users\jo136\OneDrive\FinanceData\finance.duckdb"
 
 def get_con():
     return duckdb.connect(DB_PATH, read_only=False)
+
+# --- EXAMPLE: run SQL here ---
+sql = """
+ALTER TABLE security_dim ADD COLUMN federal_taxable_ratio DOUBLE;
+ALTER TABLE security_dim ADD COLUMN state_taxable_ratio DOUBLE;
+"""
+
+with get_con() as con:
+    con.execute(sql)
+    print(df)
